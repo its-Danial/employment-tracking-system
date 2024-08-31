ca8c7ee3a2de943d8f44655b8f758282dd5a7159
its-Danial <itsmalikdanial@gmail.com>
2024-08-31 21:57:05 +0500

    added controllers

---
+++ app/controllers/auth/forgot_password_controller.ts
@@ -0,0 +1,7 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class ForgotPasswordsController {
+  async index(ctx: HttpContext) {
+    throw new Error('Method not implemented.')
+  }
+}
