82fae3444c33d80ca4e5fd8203226044c99c5c78
its-Danial <itsmalikdanial@gmail.com>
2024-08-31 21:59:19 +0500

    added auth get routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,20 @@
 */
 import router from '@adonisjs/core/services/router'
 
+import { middleware } from './kernel.js'
+
 const HomeController = () => import('#controllers/home_controller')
+const LoginController = () => import('#controllers/auth/login_controller')
+const RegistersController = () => import('#controllers/auth/register_controller')
+const ForgotPasswordsController = () => import('#controllers/auth/forgot_password_controller')
 
 router.get('/', [HomeController, 'index'])
+
+router
+  .group(() => {
+    router.get('login', [LoginController, 'index']).as('login')
+    router.get('register', [RegistersController, 'index']).as('register')
+    router.get('forgot-password', [ForgotPasswordsController, 'index']).as('forgot-password')
+  })
+  .as('auth')
+  .use(middleware.guest())
