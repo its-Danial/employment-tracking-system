ae6cf4759d7e1eb62dd7d35b4338924b4c2fe734
its-Danial <itsmalikdanial@gmail.com>
2024-09-02 01:00:55 +0500

    add app layout

--- resources/inertia/app/ssr.ts
+++ resources/inertia/app/ssr.ts
@@ -3,6 +3,8 @@ import { renderToString } from '@vue/server-renderer'
 
 import { type DefineComponent, createSSRApp, h } from 'vue'
 
+import AppLayout from '~/layouts/app_layout.vue'
+
 export default function render(page: any) {
   return createInertiaApp({
     page,
@@ -11,7 +13,10 @@ export default function render(page: any) {
       const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue', {
         eager: true,
       })
-      return pages[`../pages/${name}.vue`]
+      const page = pages[`../pages/${name}.vue`]
+
+      page.default.layout = page.default.layout || AppLayout
+      return page
     },
 
     setup({ App, props, plugin }) {
