6c9d7a3c75e75052fe5cc8b3ded20ecb2332df0f
its-Danial <itsmalikdanial@gmail.com>
2024-09-01 14:43:19 +0500

    fix component

--- resources/inertia/components/base/sheet/SheetHeader.vue
+++ resources/inertia/components/base/sheet/SheetHeader.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 
+import { DialogTitle } from 'radix-vue'
+
 import { cn } from '#lib/utils'
 
 const props = defineProps<{ class?: HTMLAttributes['class'] }>()
 </script>
 
 <template>
-  <div :class="cn('flex flex-col gap-y-2 text-center sm:text-left', props.class)">
+  <DialogTitle :class="cn('flex flex-col gap-y-2 text-center sm:text-left', props.class)">
     <slot />
-  </div>
+  </DialogTitle>
 </template>
