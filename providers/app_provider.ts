ffc9a7c063f784b2b75304efc2501fc056d32fae
its-Danial <itsmalikdanial@gmail.com>
2024-09-01 12:45:12 +0500

    register request extension

---
+++ providers/app_provider.ts
@@ -0,0 +1,11 @@
+import type { ApplicationService } from '@adonisjs/core/types'
+
+export default class AppProvider {
+  constructor(protected app: ApplicationService) {}
+  /**
+   * The container bindings have booted
+   */
+  async boot() {
+    await import('#extensions/http_context')
+  }
+}
