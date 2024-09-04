e6f52247051f60f01797090b5ea493f27a305a4f
its-Danial <itsmalikdanial@gmail.com>
2024-09-04 09:18:02 +0500

    tenant

--- app/models/tenant.ts
+++ app/models/tenant.ts
@@ -1,14 +1,29 @@
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 
 import { DateTime } from 'luxon'
+
+import User from './user.js'
 
 export default class Tenant extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare title: string
+
+  @column()
+  declare subdomain: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => User)
+  declare users: HasMany<typeof User>
 }
