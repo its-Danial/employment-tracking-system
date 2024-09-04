3895d5c895bbc300d0c4ef6c184513a341d6e8fc
its-Danial <itsmalikdanial@gmail.com>
2024-09-04 09:13:03 +0500

    added form

--- resources/inertia/pages/auth/login.vue
+++ resources/inertia/pages/auth/login.vue
@@ -7,11 +7,22 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3'
-import { Button } from '~/components/base/button'
-import { Checkbox } from '~/components/base/checkbox'
-import { Input } from '~/components/base/input'
-import { Label } from '~/components/base/label'
+import { Link, useForm } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+const form = useForm('LoginUser', {
+  email: '',
+  password: '',
+  rememberMe: false,
+})
+
+const loading = ref(false)
+
+const handleSubmit = () => {
+  loading.value = true
+  form.post('/login')
+  loading.value = false
+}
 </script>
 
 <template>
@@ -22,10 +33,10 @@ import { Label } from '~/components/base/label'
         Enter your email below to login to your account
       </p>
     </div>
-    <div class="grid gap-4">
+    <form @submit.prevent="handleSubmit" class="grid gap-4">
       <div class="grid gap-2">
         <Label for="email">Email</Label>
-        <Input id="email" type="email" placeholder="m@example.com" required />
+        <Input id="email" type="email" placeholder="m@example.com" required v-model="form.email" />
       </div>
       <div class="grid gap-2">
         <div class="flex items-center">
@@ -34,10 +45,10 @@ import { Label } from '~/components/base/label'
             Forgot your password?
           </Link>
         </div>
-        <Input id="password" type="password" required />
+        <Input id="password" type="password" required v-model="form.password" />
       </div>
       <div class="flex items-center space-x-2">
-        <Checkbox id="remember-me" />
+        <Checkbox id="remember-me" v-model:checked="form.rememberMe" />
         <label
           for="remember-me"
           class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -45,11 +56,13 @@ import { Label } from '~/components/base/label'
           Remember me
         </label>
       </div>
-      <Button type="submit" class="w-full"> Login </Button>
-    </div>
+      <Button :loading type="submit" class="w-full"> Login </Button>
+    </form>
     <div class="text-center text-sm">
       Don't have an account?
       <Link href="/register" class="underline"> Register </Link>
     </div>
+
+    <p v-if="form.errors" class="text-red-500">form: {{ form.errors }}</p>
   </div>
 </template>
