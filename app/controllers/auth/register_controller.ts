dde639286dc908fd77ba2381846462315835d1b6
its-Danial <itsmalikdanial@gmail.com>
2024-09-04 09:16:30 +0500

    register

--- app/controllers/auth/register_controller.ts
+++ app/controllers/auth/register_controller.ts
@@ -1,7 +1,23 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
+import User from '#models/user'
+import { registerValidator } from '#validators/auth'
+
 export default class RegistersController {
   async index({ inertia }: HttpContext) {
     return inertia.render('auth/register')
+  }
+
+  async store({ request, response, auth, tenant }: HttpContext) {
+    const data = await request.validateUsing(registerValidator(tenant.id))
+
+    const user = await User.create({
+      ...data,
+      tenantId: tenant.id,
+    })
+
+    await auth.use('web').login(user)
+
+    response.redirect().toRoute('home.index')
   }
 }
