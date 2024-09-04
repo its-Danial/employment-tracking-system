327c00b350a0ab273005d137410ff48c453c95d8
its-Danial <itsmalikdanial@gmail.com>
2024-09-04 09:12:42 +0500

    added form

--- resources/inertia/pages/auth/register.vue
+++ resources/inertia/pages/auth/register.vue
@@ -7,10 +7,23 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3'
-import { Button } from '~/components/base/button'
-import { Input } from '~/components/base/input'
-import { Label } from '~/components/base/label'
+import { Link, useForm } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+const form = useForm('CreateUser', {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+})
+
+const loading = ref(false)
+
+const handleSubmit = () => {
+  loading.value = true
+  form.post('/register')
+  loading.value = false
+}
 </script>
 
 <template>
@@ -21,27 +34,27 @@ import { Label } from '~/components/base/label'
         Enter your information to create an account
       </p>
     </div>
-    <div class="grid gap-4">
+    <form @submit.prevent="handleSubmit" class="grid gap-4">
       <div class="grid grid-cols-2 gap-4">
         <div class="grid gap-2">
           <Label for="first-name">First name</Label>
-          <Input id="first-name" placeholder="Max" required />
+          <Input id="first-name" placeholder="Max" required v-model="form.firstName" />
         </div>
         <div class="grid gap-2">
           <Label for="last-name">Last name</Label>
-          <Input id="last-name" placeholder="Robinson" required />
+          <Input id="last-name" placeholder="Robinson" required v-model="form.lastName" />
         </div>
       </div>
       <div class="grid gap-2">
         <Label for="email">Email</Label>
-        <Input id="email" type="email" placeholder="m@example.com" required />
+        <Input id="email" type="email" placeholder="m@example.com" required v-model="form.email" />
       </div>
       <div class="grid gap-2">
         <Label for="password">Password</Label>
-        <Input id="password" type="password" />
+        <Input id="password" type="password" v-model="form.password" />
       </div>
-      <Button type="submit" class="w-full"> Create an account </Button>
-    </div>
+      <Button type="submit" class="w-full" :loading> Create an account </Button>
+    </form>
     <div class="text-center text-sm">
       Already have an account?
       <Link href="/login" class="underline"> Sign in </Link>
