e6b8aad7572abbb594672a82f347316e606d8ac6
its-Danial <itsmalikdanial@gmail.com>
2024-09-04 17:25:54 +0500

    remove feedback

--- database/factories/tenant_factory.ts
+++ database/factories/tenant_factory.ts
@@ -15,7 +15,6 @@ export const TenantFactory = factory
       title: faker.company.buzzPhrase(),
       subdomain: tenantCounter === 1 ? 'localhost' : faker.internet.domainName(), // Set 'localhost' for the first tenant
       companyWebsite: faker.internet.url(),
-      feedbackEmail: faker.internet.email().toLowerCase(),
     }
   })
   .relation('branding', () => BrandingFactory)
