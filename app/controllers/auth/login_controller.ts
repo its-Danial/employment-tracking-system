3429641e6131ab4c3f66ee4f6e4deaa3790bac30
its-Danial <itsmalikdanial@gmail.com>
2024-09-04 09:16:03 +0500

    login controller

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -1,7 +1,19 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
+import User from '#models/user'
+import { loginValidator } from '#validators/auth'
+
 export default class LoginController {
   async index({ inertia }: HttpContext) {
     return inertia.render('auth/login')
+  }
+  async store({ request, response, auth, tenant, session }: HttpContext) {
+    const { email, password, rememberMe } = await request.validateUsing(loginValidator)
+
+    const user = await User.verifyCredentials(email, password, tenant.id)
+
+    await auth.use('web').login(user, rememberMe)
+
+    return response.redirect().toRoute('home.index')
   }
 }
