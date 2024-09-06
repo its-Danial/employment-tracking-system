b1e4e7633167a49cca222b7a5feb82ae8cd61938
its-Danial <itsmalikdanial@gmail.com>
2024-09-06 09:51:30 +0500

    add destructive variant

--- resources/inertia/components/base/input/index.ts
+++ resources/inertia/components/base/input/index.ts
@@ -1,1 +1,21 @@
+import { type VariantProps, cva } from 'class-variance-authority'
+
 export { default as Input } from './Input.vue'
+
+export const inputVariants = cva(
+  'flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'border-input placeholder:text-muted-foreground focus-visible:ring-ring',
+        destructive:
+          'border-destructive placeholder:text-destructive-foreground focus-visible:ring-destructive',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+)
+
+export type InputVariants = VariantProps<typeof inputVariants>
