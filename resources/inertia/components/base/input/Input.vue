a092e2c54f08b7d517d4cd22691fe6f3219440ae
its-Danial <itsmalikdanial@gmail.com>
2024-09-06 09:51:42 +0500

    show error

--- resources/inertia/components/base/input/Input.vue
+++ resources/inertia/components/base/input/Input.vue
@@ -5,7 +5,13 @@ import type { HTMLAttributes } from 'vue'
 
 import { cn } from '#lib/utils'
 
+import { type InputVariants, inputVariants } from '.'
+
+defineOptions({ inheritAttrs: false })
+
 const props = defineProps<{
+  variant?: InputVariants['variant']
+  error?: string
   defaultValue?: string | number
   modelValue?: string | number
   class?: HTMLAttributes['class']
@@ -23,12 +29,9 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 
 <template>
   <input
+    v-bind="$attrs"
     v-model="modelValue"
-    :class="
-      cn(
-        'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-        props.class
-      )
-    "
+    :class="cn(inputVariants({ variant: error ? 'destructive' : variant }), props.class)"
   />
+  <small v-if="error" class="text-destructive">{{ error }}</small>
 </template>
