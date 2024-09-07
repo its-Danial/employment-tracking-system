3c73d91ea9137a4c33593f16c784752d7cb4ab98
its-Danial <itsmalikdanial@gmail.com>
2024-09-07 17:28:34 +0500

    fake seeder

--- database/seeders/01_fake_seeder.ts
+++ database/seeders/01_fake_seeder.ts
@@ -9,13 +9,10 @@ export default class extends BaseSeeder {
   static environment = ['development']
 
   async run() {
-    // Create tenants
     const tenants = await TenantFactory.with('branding').createMany(2)
 
-    // Create users
     const users = await UserFactory.createMany(5)
 
-    // Create skills and assign tenant IDs
     const skills = await SkillFactory.merge({ tenantId: tenants[0].id }).createMany(5)
 
     // Create candidates and dynamically assign 'createdBy' and 'tenantId' fields
@@ -23,21 +20,20 @@ export default class extends BaseSeeder {
       users.map(async (user) => {
         return CandidateFactory.merge({ createdBy: user.id, tenantId: tenants[0].id }).createMany(
           10
-        ) // Create 10 candidate per user
+        ) // 10 candidate per user
       })
     )
 
-    // Attach skills to candidates without handling tenant_id and name in the pivot table
+    // Attach skills to candidates
     await Promise.all(
       tenantCandidates.map(async (candidates) => {
-        // Get a random set of skills
         const skillIds = this.#getRandom(skills, 3).map((skill) => skill.id)
         await Promise.all(
           candidates.map(async (candidate) => {
             // Attach skills to the candidate
             await candidate.related('skills').attach(
               skillIds.reduce<Record<number, {}>>((obj, skillId) => {
-                obj[skillId] = {} // No additional fields needed
+                obj[skillId] = {}
                 return obj
               }, {})
             )
@@ -47,7 +43,6 @@ export default class extends BaseSeeder {
     )
   }
 
-  // Utility function to get random elements from an array
   #getRandom<T>(array: T[], count: number): T[] {
     const shuffled = array.sort(() => 0.5 - Math.random())
     return shuffled.slice(0, count)
