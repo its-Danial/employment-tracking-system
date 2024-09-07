83bf0ccb7fbf5ca4b8b73002233f425e6a2cffc1
its-Danial <itsmalikdanial@gmail.com>
2024-09-07 17:29:38 +0500

    add clear btn

--- resources/inertia/components/base/input/Input.vue
+++ resources/inertia/components/base/input/Input.vue
@@ -3,6 +3,8 @@ import { useVModel } from '@vueuse/core'
 
 import type { HTMLAttributes } from 'vue'
 
+import { X } from 'lucide-vue-next'
+
 import { cn } from '#lib/utils'
 
 import { type InputVariants, inputVariants } from '.'
@@ -15,23 +17,37 @@ const props = defineProps<{
   defaultValue?: string | number
   modelValue?: string | number
   class?: HTMLAttributes['class']
+  clearable?: boolean
 }>()
 
 const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
+  (e: 'click:clear'): void
 }>()
 
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
   defaultValue: props.defaultValue,
 })
+
+const resetValue = () => {
+  emits('update:modelValue', props.defaultValue || '')
+  emits('click:clear')
+}
 </script>
 
 <template>
-  <input
-    v-bind="$attrs"
-    v-model="modelValue"
-    :class="cn(inputVariants({ variant: error ? 'destructive' : variant }), props.class)"
-  />
+  <div class="relative">
+    <input
+      v-bind="$attrs"
+      v-model="modelValue"
+      :class="cn(inputVariants({ variant: error ? 'destructive' : variant }), props.class)"
+    />
+    <X
+      v-if="clearable && modelValue"
+      @click="resetValue"
+      class="absolute right-2 top-1/2 size-4 -translate-y-1/2 cursor-pointer text-muted-foreground hover:text-primary"
+    />
+  </div>
   <small v-if="error" class="text-destructive">{{ error }}</small>
 </template>
